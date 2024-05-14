37d182f83c06eafca7dcc01ebd6e02c31f3b27e2
Rajat <rajawatrajat226@gmail.com>
2024-05-14 17:39:51 +0530

    -

--- main.py
+++ main.py
@@ -63,21 +63,21 @@ subprocess.Popen(["python", "query_interface/web_ui.py"])
 
 # Find the process using port 5001:
 # bash
-# Copy code
+#
 # lsof -i :5001
 # This will display the list of processes using that port. You can then kill the process by noting its PID and running:
 # bash
-# Copy code
+# 
 # kill -9 <PID>
 # Example:
 # bash
-# Copy code
+# 
 # lsof -i :5001
 # COMMAND   PID USER   FD   TYPE             DEVICE SIZE/OFF NODE NAME
 # python3  12345 rajat   10u  IPv4 0x1c2ef45d6887340b      0t0  TCP *:commplex-link (LISTEN)
 # To stop this process:
 # bash
-# Copy code
+# 
 # kill -9 12345
 
 
